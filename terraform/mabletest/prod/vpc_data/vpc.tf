95d56dfbafa7fac22de4324ae091b7cee538052b
Russell Maclean <russ@graphenic.com.au>
2024-07-10 14:16:04 +0930

    Data VPC
    Signed-off-by: Russell Maclean <russ@graphenic.com.au>

---
+++ terraform/mabletest/prod/vpc_data/vpc.tf
@@ -0,0 +1,23 @@
+module "vpc" {
+  source                  = "terraform-aws-modules/vpc/aws"
+  version                 = "v5.9.0"
+  name                    = "${local.mt_environment}-data"
+  azs                     = local.availability_zones
+  cidr                    = local.vpc_cidr
+  map_public_ip_on_launch = false
+  enable_dns_hostnames    = false
+  enable_dns_support      = false
+  single_nat_gateway      = true
+  enable_nat_gateway      = false
+
+  private_subnets = [
+    cidrsubnet(local.vpc_cidr, 5, 0),
+    cidrsubnet(local.vpc_cidr, 5, 1),
+  ]
+
+  database_subnets = [
+    cidrsubnet(local.vpc_cidr, 5, 2),
+    cidrsubnet(local.vpc_cidr, 5, 3),
+  ]
+
+}
